a8564d8ab651b671943da6be8a2e221e4e81b4f6
huangc <huangc_ly@163.com>
2018-12-06 21:42:13 +0800

    摄像机类Demo

--- Camera.cpp
+++ Camera.cpp
@@ -1,6 +1,8 @@
 #include "Camera.h"
 #include "math.h"
+#include "Quat.h"
 
+#define DEG2RAD (PIE/180)
 
 void InitCamera(Camera &cam, int nWidth, int nHeight, double fAngleV, double fNear, double fFar)
 {
@@ -25,5 +27,50 @@ void InitCamera(Camera &cam, int nWidth, int nHeight, double fAngleV, double fNe
 	cam.position.set(0, 0, 10);
 	cam.target.set(0, 0, 0);
 	cam.v.set(0, 1, 0); // Up-vector
+
+}
+
+void CalcUVN(Camera &cam, Vector3F &att)
+{
+	float Y = sin(att.x * DEG2RAD); // att.x£ºpitch
+	float X = cos(att.x * DEG2RAD) * sin(att.z * DEG2RAD); // att.z: yaw
+	float Z = cos(att.x * DEG2RAD) * cos(att.z * DEG2RAD);
+
+	cam.n.set(X, Y, Z);
+	float len = VectorMagnitude(cam.n);
+	cam.n.set(X / len, Y / len, Z / len);
+	VectorCrossProduct(cam.n, cam.v, cam.u);
+	VectorCrossProduct(cam.n, cam.u, cam.v);
+}
+
+void SetViewMatrix(Camera &cam)
+{
+	cam.ViewMatrix.m[0] = cam.u.x;
+	cam.ViewMatrix.m[1] = cam.v.x;
+	cam.ViewMatrix.m[2] = cam.n.x;
+	cam.ViewMatrix.m[3] = 0;
+	cam.ViewMatrix.m[4] = cam.u.y;
+	cam.ViewMatrix.m[5] = cam.v.y;
+	cam.ViewMatrix.m[6] = cam.n.y;
+	cam.ViewMatrix.m[7] = 0;
+	cam.ViewMatrix.m[8] = cam.u.z;
+	cam.ViewMatrix.m[9] = cam.v.z;
+	cam.ViewMatrix.m[10] = cam.n.z;
+	cam.ViewMatrix.m[11] = 0;
+	cam.ViewMatrix.m[12] = -VectorDotProduct(cam.u, cam.position);
+	cam.ViewMatrix.m[13] = -VectorDotProduct(cam.v, cam.position);
+	cam.ViewMatrix.m[14] = -VectorDotProduct(cam.n, cam.position);
+	cam.ViewMatrix.m[15] = 1;
+}
+
+void UpdateCamera(Camera &cam, Vector3F &pos, Vector3F &att)
+{
+	cam.position.set(pos.x, pos.y, pos.z);
+	CalcUVN(cam, att);
+	SetViewMatrix(cam);
+}
+
+void UpdateFrustum()
+{
 
 }
--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 #include "Quat.h"
 #include <math.h>
 #include <Windows.h>
-#include "..\..\Include\glut.H"
+#include "glut.H"
 #include "Camera.h"
 
 Camera g_camera;
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
+	InitCamera(g_camera, 800, 600, 60, 1, 100);
+	glViewport(0, 0, 800, 600);
+}
+
 
 void SVSPreRender(void)
 {
@@ -20,16 +29,37 @@ void SVSPreRender(void)
 	glLoadIdentity();
 
 	// update camera matrix
-	UpdateCameraMatrix(&g_camera, );
+	UpdateCamera(g_camera, Vector3F(0, 0, 50), Vector3F(10, 0, 0));
 
 	// set camera matrix
 	glMultMatrixf(g_camera.ViewMatrix.m);
 }
 
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+
+	SVSPreRender();
+
+	glPushMatrix();
+	glutWireSphere(1.0, 20, 16);   /* draw sun */
+
+	glPopMatrix();
+	glFlush();
+}
 
 
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 27:
+		exit(0);
+		break;
+	}
+}
 
-int main()
+int main(int argc, char** argv)
 {
 #if 0
 	Quat result;
@@ -43,11 +73,22 @@ int main()
 	QUAT_Rotate(axis, PIE / 2.0, Vector3F(0.707, 0.707, 0), result);
 
 	printf("r: %f\ta: %f\tb: %f\tc: %f\n", result.r, result.v.x, result.v.y, result.v.z);
-#endif
+
 
 	InitCamera(g_camera, 800, 600, 40, 1.0, 1000.0);
+#endif
 
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
 
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
 
-	getchar();
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+	return 0;
+
 }
